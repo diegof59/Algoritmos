1d118eb379bc7512a4f7dcc1aa3eca65b683a1b0
Diego Botero Celi <diego.botero@correounivalle.edu.co>
2020-01-22 22:13:34 -0500

    Conteo de inversiones en un vector

---
+++ countInversions.cpp
@@ -0,0 +1,108 @@
+
+// Implementacion del algoritmo intuitivo para hallar las inversiones de un arreglo
+void invCountIntuitivo(vector<int> arr, int n){
+
+    int count = 0;
+
+    for(int i = 0; i < n; i++){
+
+        for (int j = i+1; j < n; j++){
+
+            if (arr [i] > arr[j]){
+                count++;
+            }
+        }
+    }
+
+    cout << count << endl;
+}
+
+// Implementacion del algoritmo para hallar las inversiones de un arreglo utilizando tecnica divide y venceras
+// Basado en mergesort
+int mergeSortInvCount(vector<int> &arr, int izq, int der);
+int mergeInvCount(vector<int> &arr, int izq, int mitad, int der);
+
+void invCountRecursivo(vector<int> &arr, int n){
+
+    int numInversiones = mergeSortInvCount(arr, 1, n);
+    cout << "Num inversiones:" << numInversiones << endl;
+    for(int i=0; i < n; i++){
+
+        cout<<arr[i]<<endl;
+    }
+}
+
+int mergeSortInvCount(vector<int> &arr, int izq, int der){
+
+    int invCount = 0;
+
+    if(izq < der){
+
+        int mitad = (izq + der)/2;
+        invCount = mergeSortInvCount(arr, izq, mitad);
+        invCount += mergeSortInvCount(arr, mitad+1, der);
+        invCount += mergeInvCount(arr, izq, mitad, der);
+    }
+
+    return invCount;
+}
+
+int infinito = numeric_limits<int>::max();
+
+int mergeInvCount(vector<int> &arr, int izq, int mitad, int der){
+
+    int n1 = mitad - izq + 1;
+    int n2 = der - mitad;
+
+    int invCount = 0;
+
+    vector<int> vectorIzq;
+    vector<int> vectorDer;
+
+    for(int k=0;k<n1;k++){
+
+        vectorIzq.push_back(arr[izq+k-1]);
+    }
+
+    vectorIzq.push_back(infinito);
+
+    for(int k=0;k<n2;k++){
+
+        vectorDer.push_back(arr[mitad+k]);
+    }
+
+    vectorDer.push_back(infinito);
+
+    int i = 0;
+    int j = 0;
+
+    for(int k = izq; k <= der; k++){
+
+        if(vectorIzq[i] <= vectorDer[j]){
+
+            arr[k-1] = vectorIzq[i];
+            i++;
+        }
+        else{
+
+            arr[k-1] = vectorDer[j];
+            j++;
+            invCount += (n1 - i);
+        }
+    }
+
+    return invCount;
+}
+
+// main
+int main(){
+
+    cout << "Conteo de inversiones, algoritmo intuitivo." << endl;
+    invCountIntuitivo({4,3,1,8,2}, 5);
+
+    cout << "Conteo de inversiones, algoritmo recursivo." << endl;
+    vector<int> v = {4,3,1,8,2};
+    invCountRecursivo(v, 5);
+
+    return 0;
+}
